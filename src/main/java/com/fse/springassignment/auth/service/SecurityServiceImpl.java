905456d33637bca4e1f290643a7d19ba898b0577
Anirban Majumdar <anirbanmajumdar@Manass-iMac.local>
2019-05-27 23:10:48 +0530

    third commit

--- src/main/java/com/fse/springassignment/auth/service/SecurityServiceImpl.java
+++ src/main/java/com/fse/springassignment/auth/service/SecurityServiceImpl.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -33,8 +34,17 @@ public class SecurityServiceImpl implements SecurityService {
 
 	@Override
 	public void autoLogin(String username, String password) {
-		// TODO Auto-generated method stub
+		UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+		UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
+				userDetails, password, userDetails.getAuthorities());
 
+		authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+
+		if (usernamePasswordAuthenticationToken.isAuthenticated()) {
+			SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+			logger.debug(String.format("Auto login %s successfully!", username));
+
+		}
 	}
 
 }
